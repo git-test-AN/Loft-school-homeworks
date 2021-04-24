26c8bbe2f8f848eb18fab3b7c78395ff70abcc4d
unknown <bleach4321@yandex.ru>
2021-04-24 10:06:18 +0400

    Work-1 Task-4 сдÐÐделано, но с костыÐлем

--- Work-1/Task-4.php
+++ Work-1/Task-4.php
@@ -1,16 +1,17 @@
+<pre>
 <?php
 
 $day = rand(-3, 14);
-echo "Текущий день на этой неделе: $day";
-echo "<pre>";
-switch ($day) {
+$day = rand(-1, 1);
+echo "Текущий день на этой неделе: $day\n";
+switch (true) {
     case ($day >= 1 && $day <= 5):
         echo "Это рабочий день";
         break;
     case ($day >= 6 && $day <= 7):
         echo "Это выходной день";
         break;
-    case ($day >= -3 && $day <= 14):
+    default:
         echo "Неизвестный день";
         break;
 }
