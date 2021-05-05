01050ee643b6c9bc91b15a8310edd1a1c736761a
unknown <bleach4321@yandex.ru>
2021-05-05 09:16:02 +0400

    Work-3 Task-1 исправлен и работает

---
+++ Week-2/Work-3/index.php
@@ -0,0 +1,157 @@
+<pre>
+<?php
+
+//function task1() {
+//
+//    }
+//    return;
+//echo task1();
+
+/**Task-3.1**/
+//$id = range(1, 50);
+//
+//$name = [
+//        'Vitya',
+//        'Katya',
+//        'Sibila',
+//        'Abraam',
+//        'Dolores',
+//];
+//$task = [];
+//for ($i = 1; $i <= 50; $i++) {
+//    $id = $i;
+//    $task[] = [
+//        'id' => "$id",
+//        'name' => $name[rand(0, 4)],
+//        'age' => mt_rand(18, 45),
+//    ];
+//}
+//$json = json_encode($task);
+//echo PHP_EOL , $json;
+//
+//file_put_contents('../users.json', $json);
+//
+//$another_json = json_decode($json, true);
+//
+//$names = [];
+//$sumAge = 0;
+//foreach ($another_json as $task) {
+//    if (isset($names[$task['name']])) {
+//        $names[$task['name']]++;
+//    } else {
+//        $names[$task['name']] = 1;
+//    }
+//    $sumAge += $task['age'];
+//}
+//var_dump($names);
+//echo "Средний возраст: " . ($sumAge / sizeof($another_json));
+
+/**searching-names-failed**/
+//echo '<br>';
+//$search_array = $another_json;
+//if (array_key_exists("Dolores", $search_array)) {
+//    echo "Массив содержит элемент 'first'.";
+//} else {
+//    echo "Нет в массиве нифига";
+//}
+//echo '<br>';
+//print_r(array_count_values($name));
+
+/**Task-3.1**/
+//<form action="index.php" method="post">
+//<p>Ваше имя: <input type="text" name="name"></p>
+//<p>Ваш возраст: <input type="text" name="age"></p>
+//<p><input type="submit"></p>
+//</form>
+//
+// <section class="section order">
+//          <div class="container">
+//            <div class="section__title">Закажите доставку</div>
+//            <div class="order__form">
+//              <form class="order__form-tag" id="order-form">
+//                <div class="order__form-col">
+//                  <div class="order__form-row order__form-row_double">
+//                    <label class="order__form-block">
+//                      <div class="order__form-label">Имя</div>
+//                      <input class="order__form-input" name="name" type="text" placeholder="">
+//                    </label>
+//                    <label class="order__form-block">
+//                      <div class="order__form-label">Телефон</div>
+//                      <input class="order__form-input phone-mask" name="phone" type="text" placeholder="">
+//                    </label>
+//                  </div>
+//                  <div class="order__form-row order__form-row_double">
+//                    <label class="order__form-block">
+//                      <div class="order__form-label">email</div>
+//                      <input class="order__form-input" name="email" type="email" placeholder="">
+//                    </label>
+//                    <label class="order__form-block">
+//                      <div class="order__form-label">Улица</div>
+//                      <input class="order__form-input" name="street" type="text" placeholder="">
+//                    </label>
+//                  </div>
+//                  <div class="order__form-row order__form-row_quatro">
+//                    <label class="order__form-block">
+//                      <div class="order__form-label">Дом</div>
+//                      <input class="order__form-input" name="home" type="text" placeholder="">
+//                    </label>
+//                    <label class="order__form-block">
+//                      <div class="order__form-label">Корпус</div>
+//                      <input class="order__form-input" name="part" type="text" placeholder="">
+//                    </label>
+//                    <label class="order__form-block">
+//                      <div class="order__form-label">Квартира</div>
+//                      <input class="order__form-input" name="appt" type="text" placeholder="">
+//                    </label>
+//                    <label class="order__form-block">
+//                      <div class="order__form-label">Этаж</div>
+//                      <input class="order__form-input" name="floor" type="text" placeholder="">
+//                    </label>
+//                  </div>
+//                </div>
+//                <div class="order__form-col">
+//                  <div class="order__form-row">
+//                    <label class="order__form-block">
+//                      <div class="order__form-label">Комментарий</div>
+//                      <textarea class="order__form-input order__form-input_textarea" name="comment"></textarea>
+//                    </label>
+//                  </div>
+//                  <div class="order__form-buttons">
+//                    <div class="order__form-row">
+//                      <label class="order__radio">
+//                        <input class="order__radio-elem" name="payment" type="radio">
+//                        <div class="order__radio-fake"></div>
+//                        <div class="order__radio-title">Потребуется сдача</div>
+//                      </label>
+//                      <label class="order__radio">
+//                        <input class="order__radio-elem" name="payment" type="radio">
+//                        <div class="order__radio-fake"></div>
+//                        <div class="order__radio-title">Оплата по карте</div>
+//                      </label>
+//                    </div>
+//                    <div class="order__form-row">
+//                      <label class="order__radio order__radio_checkbox">
+//                        <input class="order__radio-elem" name="callback" type="checkbox">
+//                        <div class="order__radio-fake"></div>
+//                        <div class="order__radio-title">Не перезванивать</div>
+//                      </label>
+//                    </div>
+//                    <div class="order__form-row">
+//                      <input class="order__form-button" name="" type="submit" value="Заказать">
+//                      <input class="order__form-button order__form-button_reset" name="" type="reset" value="Очистить">
+//                    </div>
+//                  </div>
+//                </div>
+//              </form>
+//            </div>
+//          </div>
+//        </section>
+
+/**Task-4**/
+
+
+
+
+
+
+
