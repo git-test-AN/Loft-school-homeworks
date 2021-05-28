3d53ca392eae438957fc41d0a778882e69c6076d
unknown <bleach4321@yandex.ru>
2021-05-28 14:10:50 +0400

    Work-3, Work-4, Work-5 and Work-6 in progress

---
+++ Week-3/Vypusknaya-rapota-1/base/config.php
@@ -0,0 +1,13 @@
+<?php
+const DB_USER = 'mvc';
+const DB_NAME = 'mvc';
+const DB_HOST = 'mysql';
+const DB_PASSWORD = 'mvc';
+
+const ADMIN_IDS = [9];
+
+function d(...$args)
+{
+    var_dump($args);
+    die;
+}
---
+++ Week-5/Work-6/base/config.php
@@ -0,0 +1,13 @@
+<?php
+const DB_USER = 'loftschools';
+const DB_NAME = 'loftschools';
+const DB_HOST = 'mysql';
+const DB_PASSWORD = 'loftschools';
+
+const ADMIN_IDS = [26];
+
+function d(...$args)
+{
+    var_dump($args);
+    die;
+}
